96951ea56eb1984c61d6940e3c15776fa8e02b38
zeynel abidin <49595767+zagbey@users.noreply.github.com>
2024-01-09 15:30:55 +0300

    IServices

---
+++ NLayerApp/NLayer.Core/Services/IService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Core.Services
+{
+    public interface IService<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();
+
+        //productRepository.where(x=>x.id>5).OrderBy.ToListAsync();
+        IQueryable<T> Where(Expression<Func<T, bool>> expression);
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+
+        //update ve delete EFcore tarafında sürekli takip edildiği için stati modified oalrak değişiyor yani memoryde bir Async durum yok 
+        Task UpdateAsync(T entity);
+        Task RemoveAsync(T entity);
+        Task RemoveRangeAsync(IEnumerable<T> entities);
+    }
+}
