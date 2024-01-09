870516359d795b1e2a6763418cb0de2f055b4cab
zeynel abidin <49595767+zagbey@users.noreply.github.com>
2024-01-09 15:13:49 +0300

    IGenericRepository

---
+++ NLayerApp/NLayer.Core/Repository/IGenericRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Core.Repository
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T>GetByIdAsync(int id);
+        IQueryable<T> GetAll(Expression<Func<T,bool>> expression);
+
+        //productRepository.where(x=>x.id>5).OrderBy.ToListAsync();
+        IQueryable<T> Where(Expression<Func<T,bool>> expression);
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+
+        //update ve delete EFcore tarafında sürekli takip edildiği için stati modified oalrak değişiyor yani memoryde bir Async durum yok 
+        void Update(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+    }
+}
