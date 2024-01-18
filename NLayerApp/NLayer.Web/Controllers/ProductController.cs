bc51db5160283971c47b4efe928f1b7e7f4d7778
zeynel abidin <49595767+zagbey@users.noreply.github.com>
2024-01-18 20:54:23 +0300

    ProductController-Mvc

---
+++ NLayerApp/NLayer.Web/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NLayer.Core.Services;
+
+namespace NLayer.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _service;
+
+        public ProductController(IProductService service)
+        {
+            _service = service;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _service.GetProductsWithCategory());
+        }
+    }
+}
