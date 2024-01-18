c5eaa71dcaf20097dd93d633951be03bada3a5ea
zeynel abidin <49595767+zagbey@users.noreply.github.com>
2024-01-18 22:25:33 +0300

    Pc Save

--- NLayerApp/NLayer.Web/Controllers/ProductsController.cs
+++ NLayerApp/NLayer.Web/Controllers/ProductsController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using NLayer.Core.DTOs;
+using NLayer.Core.Models;
 using NLayer.Core.Services;
 
 namespace NLayer.Web.Controllers
@@ -6,15 +10,46 @@ namespace NLayer.Web.Controllers
     public class ProductsController : Controller
     {
         private readonly IProductService _service;
+        private readonly IMapper _mapper;
+        private readonly ICategoryService _categoryService;
 
-        public ProductsController(IProductService service)
+        public ProductsController(IProductService service, ICategoryService categoryService, IMapper mapper)
         {
             _service = service;
+            _categoryService = categoryService;
+            _mapper = mapper;
         }
 
         public async Task<IActionResult> Index()
         {
             return View(await _service.GetProductsWithCategory());
+        }
+
+        public async Task<IActionResult> Save()
+        {
+            var categories = _categoryService.GetAllAsync();
+
+            var categoriesDto = _mapper.Map<List<CategoryDto>>(categories);
+
+            ViewBag.categories = new SelectList(categoriesDto, "Id", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(ProductDto productDto)
+        {
+            if (ModelState.IsValid)
+            {
+                await _service.AddAsync(_mapper.Map<Product>(productDto));
+                return RedirectToAction(nameof(Index));
+            }
+
+            var categories = _categoryService.GetAllAsync();
+
+            var categoriesDto = _mapper.Map<List<CategoryDto>>(categories);
+
+            ViewBag.categories = new SelectList(categoriesDto, "Id", "Name");
+            
         }
     }
 }
