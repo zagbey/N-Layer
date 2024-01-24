9b58ccc8316c68b136a763c6d44436c45da5b857
zeynel abidin <49595767+zagbey@users.noreply.github.com>
2024-01-24 21:02:40 +0300

    CreatedDate/UpdatedDate

--- NLayerApp/NLayer.Repository/AppDbContext.cs
+++ NLayerApp/NLayer.Repository/AppDbContext.cs
@@ -14,7 +14,67 @@ namespace NLayer.Repository
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductFeature> ProductFeatures { get; set; }
 
+        public override int SaveChanges()
+        {
+            foreach (var item in ChangeTracker.Entries())
+            {
+                if (item.Entity is BaseEntity entityReference)
+                {
+                    switch (item.Entity)
+                    {
+                        case EntityState.Added:
+                            {
+                                entityReference.CreatedDate = DateTime.Now;
+                                break;
+                            }
+                        case EntityState.Modified:
+                            {
+                                entityReference.UpdateDate = DateTime.Now;
+                                break;
+                            }
 
+
+                    }
+                }
+
+
+            }
+
+
+            return base.SaveChanges();
+        }
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+
+            foreach (var item in ChangeTracker.Entries())
+            {
+                if (item.Entity is BaseEntity entityReference)
+                {
+                    switch (item.State)
+                    {
+                        case EntityState.Added:
+                            {
+                                entityReference.CreatedDate = DateTime.Now;
+                                break;
+                            }
+                        case EntityState.Modified:
+                            {
+                                Entry(entityReference).Property(x => x.CreatedDate).IsModified = false;
+
+                                entityReference.UpdateDate = DateTime.Now;
+                                break;
+                            }
+
+
+                    }
+                }
+
+
+            }
+
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             //GetExecutingAssembly çalışmış olduğum assmbly i tara yani Configuration dosyam 
